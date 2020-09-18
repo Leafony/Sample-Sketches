f851160a966e20e280914c0bb53f4655c5b84e03
Leafony <52395367+Leafony@users.noreply.github.com>
2020-09-18 13:17:18 +0900

    Add files via upload

--- ESP32_GoogleSheets/google_scripts_code.js
+++ ESP32_GoogleSheets/google_scripts_code.js
@@ -1,31 +1,10 @@
-//=====================================================================
-//  Leafony Platform sample sketch
-//     Platform     : ESP32
-//     Processor    : ESP32-WROOM-32
-//     Application  : Sending Sensor Data to Google Sheets
-//
-//     Leaf configuration
-//       (1) AP02 ESP MCU
-//       (2) AI01 4-Sensors
-//
-//    (c) 2020 Trillion-Node Study Group
-//    Released under the MIT license
-//    https://opensource.org/licenses/MIT
-//
-//      Rev.00 2020/8/17  First release
-//=====================================================================
-
-// Paste following codes to Google Scripts Editor
-
-
-// doGet is executed when accessed with GET
 function doGet(e) {
 
     let id = 'google_sheets_id'; 
     let sheetName = 'sheet_name'; 
     var result;
 
-    // e.parameter has received GET parameters, i.e. temperature, humidity, brightness
+    // e.parameter has received GET parameters, i.e. temperature, humidity, illumination
     if (e.parameter == undefined) {
         result = 'Parameter undefined';
     } else {
@@ -35,9 +14,10 @@ function doGet(e) {
 
         // get current time
         rowData[0] = new Date();
-        rowData[1] = e.parameter.temperature;
-        rowData[2] = e.parameter.humidity;
-        rowData[3] = e.parameter.brightness;
+        rowData[1] = e.parameter.UniqueID;
+        rowData[2] = e.parameter.temperature;
+        rowData[3] = e.parameter.humidity;
+        rowData[4] = e.parameter.illumination;
 
         // 1 x rowData.length cells from (newRow, 1) cell are specified
         var newRange = sheet.getRange(newRow, 1, 1, rowData.length);
