37cec93dbbd08f6a593972ca95ec0a10af729ff0
Leafony <52395367+Leafony@users.noreply.github.com>
2021-06-30 14:13:21 +0900

    Add files via upload

--- ESP32_Sound_Level_Meter/ESP32_Sound_Level_Meter.ino
+++ ESP32_Sound_Level_Meter/ESP32_Sound_Level_Meter.ino
@@ -1,11 +1,11 @@
 //=====================================================================
 //  ESP32 Sound level meter
 //
-//    (c) 2020 Trillion-Node Study Group
+//    (c) 2021 LEAFONY SYSTEMS Co., Ltd
 //    Released under the MIT license
 //    https://opensource.org/licenses/MIT
 //
-//      Rev.00 2020/08/20  First release
+//      Rev.00 2021/04/01  First release
 //=====================================================================
 
 // LED4-6:VR level display
