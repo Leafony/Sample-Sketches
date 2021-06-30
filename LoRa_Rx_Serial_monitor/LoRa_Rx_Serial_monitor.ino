cf05297b9b376a23101a1eb49ccf74c70aa9af89
Leafony <52395367+Leafony@users.noreply.github.com>
2021-06-30 14:24:09 +0900

    Add files via upload

--- LoRa_Rx_Serial_monitor/LoRa_Rx_Serial_monitor.ino
+++ LoRa_Rx_Serial_monitor/LoRa_Rx_Serial_monitor.ino
@@ -9,11 +9,11 @@
 //       (2) AI01 4-Sensors
 //       (3) AP01 AVR MCU
 //
-//		(c) 2020  Trillion-Node Study Group
+//		(c) 2021 LEAFONY SYSTEMS Co., Ltd
 //		Released under the MIT license
 //		https://opensource.org/licenses/MIT
 //
-//      Rev.00 2019/08/01  First release
+//      Rev.00 2021/04/01  First release
 //=====================================================================
 
 //=====================================================================
