57a6c6ad94aa07dc7bea5e868d219ad7276ff0d9
Leafony <52395367+Leafony@users.noreply.github.com>
2021-06-30 14:19:02 +0900

    Add files via upload

--- Illuminance_Meter/Illuminance_Meter.ino
+++ Illuminance_Meter/Illuminance_Meter.ino
@@ -1,11 +1,11 @@
 //=====================================================================
 //  Illuminance meter
 //
-//    (c) 2020 Trillion-Node Study Group
+//    (c) 2021 LEAFONY SYSTEMS Co., Ltd
 //    Released under the MIT license
 //    https://opensource.org/licenses/MIT
 //
-//      Rev.00 2020/05/05  First release
+//      Rev.00 2021/04/01  First release
 //=====================================================================
 #include <Wire.h>
 #include <ClosedCube_OPT3001.h>
