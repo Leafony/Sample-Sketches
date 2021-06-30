80f1aa6a492097b542211c5105e9ec6cc70aebaa
Leafony <52395367+Leafony@users.noreply.github.com>
2021-06-30 14:40:43 +0900

    Add files via upload

--- STM32_Human_Sensing/STM32_Human_Sensing.ino
+++ STM32_Human_Sensing/STM32_Human_Sensing.ino
@@ -1,11 +1,11 @@
 //=====================================================================
 //  STM32 Human Sensing
 //
-//    (c) 2021 Trillion-Node Study Group
+//    (c) 2021 LEAFONY SYSTEMS Co., Ltd
 //    Released under the MIT license
 //    https://opensource.org/licenses/MIT
 //
-//      Rev.00 2021/01/26  First release
+//      Rev.00 2021/04/01  First release
 //=====================================================================
 #include <Wire.h>
 
