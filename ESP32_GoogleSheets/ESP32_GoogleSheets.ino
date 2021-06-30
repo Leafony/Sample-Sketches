d91c38d049e0ff796084fe3d879ef29cc28664b2
Leafony <52395367+Leafony@users.noreply.github.com>
2021-06-30 14:09:59 +0900

    Add files via upload

--- ESP32_GoogleSheets/ESP32_GoogleSheets.ino
+++ ESP32_GoogleSheets/ESP32_GoogleSheets.ino
@@ -8,11 +8,11 @@
 //       (1) AP02 ESP MCU
 //       (2) AI01 4-Sensors
 //
-//    (c) 2020 Trillion-Node Study Group
+//    (c) 2021 LEAFONY SYSTEMS Co., Ltd
 //    Released under the MIT license
 //    https://opensource.org/licenses/MIT
 //
-//      Rev.00 2020/8/17  First release
+//      Rev.00 2021/04/01  First release
 //=====================================================================
 #include <Arduino.h>
 #include <Wire.h>
