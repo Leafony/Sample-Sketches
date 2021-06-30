2a663f1d6b2aadb4a1beed3834205737d8915878
Leafony <52395367+Leafony@users.noreply.github.com>
2021-06-30 14:17:57 +0900

    Add files via upload

--- Human_Sensing/Human_Sensing.ino
+++ Human_Sensing/Human_Sensing.ino
@@ -1,11 +1,11 @@
 //=====================================================================
 //  Human Sensing
 //
-//    (c) 2020 Trillion-Node Study Group
+//    (c) 2021 LEAFONY SYSTEMS Co., Ltd
 //    Released under the MIT license
 //    https://opensource.org/licenses/MIT
 //
-//      Rev.00 2020/05/16  First release
+//      Rev.00 2021/04/01  First release
 //=====================================================================
 #include <Wire.h>
 
