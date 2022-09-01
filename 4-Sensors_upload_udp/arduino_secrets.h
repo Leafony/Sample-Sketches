a13d92d1606748e8ff0f3324c491ccb6fc015c0b
Leafony <52395367+Leafony@users.noreply.github.com>
2022-09-01 15:15:34 +0900

    Delete 4-Sensors_upload_udp directory

--- 4-Sensors_upload_udp/arduino_secrets.h
+++
@@ -1,3 +0,0 @@
-#define SECRET_GPRS_APN "soracom.io" // replace your GPRS APN
-#define SECRET_GPRS_LOGIN "sora"     // replace with your GPRS login
-#define SECRET_GPRS_PASSWORD "sora"  // replace with your GPRS password
