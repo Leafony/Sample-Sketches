b86e949ce16da3ca3b067b651378eeb3e148c39a
Leafony <52395367+Leafony@users.noreply.github.com>
2023-01-25 13:55:55 +0900

    Update Blink.ino

--- Blink/Blink.ino
+++ Blink/Blink.ino
@@ -9,13 +9,13 @@
 //=====================================================================
 
 void setup() {
-  // LEDピンを出力ピンに設定
+  // initialize digital pin LED_BUILTIN as an output.
   pinMode(LED_BUILTIN, OUTPUT);
 }
 
 void loop() {
-  digitalWrite(LED_BUILTIN, HIGH);   // LEDを点灯
-  delay(1000);                       // 1秒待つ
-  digitalWrite(LED_BUILTIN, LOW);    // LEDを消灯
-  delay(1000);                       // 1秒待つ
+  digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(1000);                       // wait for a second
+  digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
+  delay(1000);                       // wait for a second
 }
