c50dc8d833dbb687f32d3b10638e58ca0a301eb2
Leafony <52395367+Leafony@users.noreply.github.com>
2025-08-08 15:45:54 +0900

    Add files via upload

---
+++ STM32_LTE-M_4-Sensors/arduino_secrets.h
@@ -0,0 +1,3 @@
+#define SECRET_GPRS_APN "soracom.io" // replace your GPRS APN
+#define SECRET_GPRS_LOGIN "sora"     // replace with your GPRS login
+#define SECRET_GPRS_PASSWORD "sora"  // replace with your GPRS password
